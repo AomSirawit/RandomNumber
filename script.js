62c9d7394f49207915b3fc24c59806dc55235eea
AomSirawit <makdeesirawit@gmail.com>
2023-05-30 01:29:16 +0700

    deploy
    deploy

---
+++ script.js
@@ -0,0 +1,14 @@
+let generateBtn = document.getElementById("generate");
+
+function generateNumber(){
+    let min = document.getElementById("min-input");
+    let max = document.getElementById("max-input");
+    let minValue = Number(min.value);
+    let maxValue = Number(max.value);
+
+    let result = document.getElementById("result");
+    let randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    result.textContent = randomNumber;
+
+}
+generateBtn.addEventListener("click", generateNumber);
